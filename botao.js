a8a1a4359657346b78687ef0030930224cbacb3c
Michael Moranis <97197020+MichaelMoranis@users.noreply.github.com>
2022-07-15 11:43:21 -0300

    Create botao.js

---
+++ botao.js
@@ -0,0 +1,10 @@
+const btnMobile = document.querySelector("button");
+
+function toggleMenu() {
+   const nav = document.querySelector(".nav");
+   nav.classList.toggle('active');
+}
+
+
+btnMobile.addEventListener('click', toggleMenu);
+
